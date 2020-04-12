6e7cae18e8ebdfdf46d7a14af9b8f68ca248f0a8
Panagiotis Papageorgiou <pkpapageorg@gmail.com>
2020-04-12 13:28:27 +0300

    changes to setup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ REPOS=`tr '\n' ' ' < vim-plugins.txt`
 pushd ${HOME}
 
 mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle;
-
+curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo "installing or updating plugins"
 for i in ${REPOS}; do
 name=`echo $i|rev|cut -d"/" -f1 |rev|sed 's/\.git//g'`
@@ -20,11 +20,13 @@ done
 echo "creating symlinks"
 if [ -f .vimrc ]; then
 cp .vimrc .vimrc.sav
+rm .vimrc
 fi
 ln -s ${REPO_DIR}/vimrc .vimrc
 
 if [ -f .bashrc ]; then
 cp .bashrc .bashrc.sav
+rm .bashrc
 fi
 ln -s ${REPO_DIR}/bashrc .bashrc
 
